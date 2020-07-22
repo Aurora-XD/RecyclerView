98fed564ae396127ca75043ad71d85113cb86753
yangchen <chen.yang1@thoughtworks.com>
2020-07-22 17:25:32 +0800

    add list actvity and onlick event to button recycle view

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.button_recycle_view) void onClick(){
+        Intent intent = new Intent(this, ListActivity.class);
+        startActivity(intent);
     }
 }
