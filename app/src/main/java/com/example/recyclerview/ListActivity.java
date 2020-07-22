9c199c10193505024668061ee19d01820e245253
yangchen <chen.yang1@thoughtworks.com>
2020-07-22 17:31:53 +0800

    add MyAdapter in ListActivity

--- app/src/main/java/com/example/recyclerview/ListActivity.java
+++ app/src/main/java/com/example/recyclerview/ListActivity.java
@@ -1,14 +1,53 @@
 package com.example.recyclerview;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class ListActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+    }
+
+
+    private class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
+
+        class MyViewHolder extends RecyclerView.ViewHolder{
+
+            public MyViewHolder(@NonNull View itemView) {
+                super(itemView);
+            }
+        }
+
+        @NonNull
+        @Override
+        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
     }
 }
